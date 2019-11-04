d9bbab33b9e3ee34eb63f16aa1f33c00895d1877
Smith <smith.mugalu@andela.com>
2019-11-04 15:50:59 +0300

    bbasic setup for autolayout programmatically has been done

---
+++ ImageViewer/Controller/MainCollectionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainCollectionViewController.swift
+//  ImageViewer
+//
+//  Created by Smithers on 04/11/2019.
+//  Copyright © 2019 Smithers. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .red
+        print("View has loaded")
+    }
+    
+}
+
+
