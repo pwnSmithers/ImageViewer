af26543b72a209a11e3aed3a57ec6dea82ee9b7b
Smith <smith.mugalu@andela.com>
2019-11-04 19:05:10 +0300

    Moya has been setup

---
+++ ImageViewer/API/NetworkingService.swift
@@ -0,0 +1,50 @@
+//
+//  NetworkingService.swift
+//  ImageViewer
+//
+//  Created by Smithers on 04/11/2019.
+//  Copyright © 2019 Smithers. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum NetworkingService{
+    case Search(term: String)
+}
+
+extension NetworkingService: TargetType {
+
+    var baseURL: URL {
+        switch self {
+        case .Search(let term):
+            let url = " https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(GlobalConstants.ApiKey)&text=\(term)&format=json&nojsoncallback=1"
+            let finalURL = url.replacingOccurrences(of: " ", with: "+")
+            return URL(string: finalURL)!
+        }
+    }
+    
+    var path: String {
+        return ""
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var sampleData: Data {
+        switch self {
+        case .Search(let term):
+            return "{'term':'\(term)'}".data(using: .utf8)!
+        }
+    }
+    
+    var task: Task {
+        return .requestPlain
+    }
+    
+    var headers: [String : String]? {
+        return ["content-type":"application/json"]
+    }
+    
+}
---
+++ ImageViewer/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ImageViewer
+//
+//  Created by Smithers on 04/11/2019.
+//  Copyright © 2019 Smithers. All rights reserved.
+//
+
+import Foundation
+
+struct GlobalConstants {
+    static let ApiKey = "073f547e067e953a932a6e64f59d5cd4"
+}
